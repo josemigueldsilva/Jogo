d62023b39c1d51cc7506b34417797071fb053cc2
josemigueldsilva <165943670+josemigueldsilva@users.noreply.github.com>
2025-04-03 16:03:28 -0300

    Add files via upload

---
+++ js/jogo.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+    var jogo = $('#area_jogo')
+    jogo.append("<div id='player' class='player'></div>");
+    jogo.append("<div id='nave' class='nave'></div>");
+    jogo.append("<div id='tanque' class='tanque'></div>");
+    jogo.append("<div id='pessoa' class='pessoa'></div>");
+})
